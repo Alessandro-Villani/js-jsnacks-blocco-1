fb354938b20bf423315ba7c6665cbb29b2a2f9b5
Alessandro-Villani <alessvill@libero.it>
2022-12-13 15:46:27 +0100

    store inputs into array + validation

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -10,3 +10,19 @@ const targetSum = document.getElementById('sum');
 //2. Set Array for number storage
 const numbers = [];
 
+//3. Ask User 10 numbers and store them into Array
+
+for(i = 0; numbers.length < 10; i++){
+    //3.1 ask user a number and store it into variable for validation
+    const number = parseInt(prompt('Inserisci un numero').trim());
+    console.log(number);
+    //3.2 Validation
+    if(isNaN(number)){
+        alert('Dato non valido');
+    }
+    //3.3 If number is valid, store into Array
+    else {
+        numbers.push(number);
+        console.table(numbers);
+    }
+}
