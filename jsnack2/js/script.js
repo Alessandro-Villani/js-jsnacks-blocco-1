bd4c5d6973585940666410515b103e36c2b9d715
Alessandro-Villani <alessvill@libero.it>
2022-12-13 16:48:35 +0100

    added input and target

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -4,3 +4,16 @@ In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, l
 
 ----------------------------------------------------------------------------------------*/
 
+//1. Add invited Array
+
+const invited = ['Nick Carraway', 'Daisy Buchanan', 'Tom Buchanan', 'Myrtle Wilson'];
+
+//2. Select input
+
+const inputName = document.getElementById('name');
+
+//3. Select target
+
+const targetMessage = document.getElementById('message');
+
+
