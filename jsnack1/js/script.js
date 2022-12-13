61e84f04538fed65dbdf5117f665d4998b65fdf4
Alessandro-Villani <alessvill@libero.it>
2022-12-13 15:21:29 +0100

    first commit

---
+++ jsnack1/js/script.js
@@ -0,0 +1,6 @@
+/*------------------------------------TRACCIA---------------------------------------------
+
+Il software deve chiedere per 10 volte all’utente di inserire un numero. (qui potete usare un prompt). Il programma stampa la somma di tutti i numeri inseriti.
+
+----------------------------------------------------------------------------------------*/
+
